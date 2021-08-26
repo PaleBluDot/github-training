ee820974c52ec9e5d3d56f5fe1fa47559a4069a2
Pavel Sanchez <23495830+PaleBluDot@users.noreply.github.com>
2021-08-25 23:11:15 -0400

    docs(site): update

--- commitizen.config.js
+++ commitizen.config.js
@@ -1,16 +1,16 @@
 module.exports = {
 	types: [
-		{ value: "feat", name: "feat:     A new feature" },
-		{ value: "fix", name: "fix:      A bug fix" },
-		{ value: "docs", name: "docs:     Documentation only changes" },
-		{ value: "chore", name: "chore:    Changes to the build process" },
+		{ value: "feat", name: "feat: ✨    A new feature" },
+		{ value: "fix", name: "fix:  🐛    A bug fix" },
+		{ value: "docs", name: "docs: 📚    Documentation only changes" },
+		{ value: "test", name: "test: 🚨    Changes to tests" },
 	],
 
 	scopes: [
-		{ name: "accounts" },
 		{ name: "admin" },
-		{ name: "exampleScope" },
-		{ name: "changeMe" },
+		{ name: "cli" },
+		{ name: "core" },
+		{ name: "site" },
 	],
 
 	allowTicketNumber: false,
@@ -18,40 +18,23 @@ module.exports = {
 	ticketNumberPrefix: "TICKET-",
 	ticketNumberRegExp: "\\d{1,5}",
 
-	// it needs to match the value for field type. Eg.: 'fix'
-	/*
-  scopeOverrides: {
-    fix: [
-      {name: 'merge'},
-      {name: 'style'},
-      {name: 'e2eTest'},
-      {name: 'unitTest'}
-    ]
-  },
-  */
-	// override the messages, defaults are as follows
+	// scopeOverrides: { fix: [{ name: "cli" }] },
+
 	messages: {
 		type: "Type of change that you're committing:",
-		scope: "\nDenote the SCOPE of this change (optional):",
-		// used if allowCustomScopes is true
+		scope: "\nDenote the SCOPE of this change:",
 		customScope: "Denote the SCOPE of this change:",
 		subject: "Write a SHORT, IMPERATIVE tense description of the change:\n",
 		body:
 			'Provide a LONGER description of the change (optional). Use "|" to break new line:\n',
-		breaking: "List any BREAKING CHANGES (optional):\n",
+		breaking: "List any BREAKING CHANGES:\n",
 		footer:
 			"List any ISSUES CLOSED by this change (optional). E.g.: #31, #34:\n",
 		confirmCommit: "Are you sure you want to proceed with the commit above?",
 	},
 
-	allowCustomScopes: true,
+	allowCustomScopes: false,
 	allowBreakingChanges: ["feat", "fix"],
-	// skip any questions you want
 	skipQuestions: ["body"],
-
-	// limit subject length
 	subjectLimit: 100,
-	// breaklineChar: '|', // It is supported for fields body and footer.
-	// footerPrefix : 'ISSUES CLOSED:'
-	// askForBreakingChangeFirst : true, // default is false
 };
