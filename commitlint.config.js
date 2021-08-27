b438a803a2aaf956de2c50d9a82aec49eadbf13f
Pavel Sanchez <23495830+PaleBluDot@users.noreply.github.com>
2021-08-27 16:34:51 -0400

    fix(core): testing to see if gitmoji takes effect

--- commitlint.config.js
+++ commitlint.config.js
@@ -5,7 +5,7 @@ const validScopes = scopes.map((scope) => scope.name);
 const scopeValidationLevel = allowCustomScopes ? 1 : 2;
 
 module.exports = {
-	extends: ["gitmoji"],
+	extends: ["./node_modules/commitlint-config-gitmoji"],
 
 	headerPattern: /^(?::\w*:|(?:\ud83c[\udf00-\udfff])|(?:\ud83d[\udc00-\ude4f\ude80-\udeff])|[\u2600-\u2B55])\s(?<type>\w*)(?:\((?<scope>.*)\))?!?:\s(?<subject>(?:(?!#).)*(?:(?!\s).))\s?(?<ticket>#\d*)?$/,
 	headerCorrespondence: ["type", "scope", "subject", "ticket"],
